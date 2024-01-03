1abff21514046204534c14621bf237ada9106654
Marco Calderon <marco.calderon1701@hotmail.com>
2024-01-03 16:28:25 -0500

    gaa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,25 @@
-
+"use client"
 import React from 'react';
 import axios from 'axios';
 
-const fetchData = async () => {
-  try {
-    const response = await axios.get('https://worldtimeapi.org/api/timezone/America/Lima');
-    const hora = response.data.utc_datetime;
-    console.log('UTC Hora:', hora);
 
-  } catch (error) {
-    console.error('Error fetching data:', error.message);
-    alert('Error fetching data');
-  }
-};
 export default function Page() {
 
+ const fetchData = async () => {
+    try {
+      const response = await axios.get('https://worldtimeapi.org/api/timezone/America/Lima');
+      const hora = response.data.utc_datetime;
+      console.log('UTC Hora:', hora);
 
-fetchData();
-const interval = setInterval(fetchData, 30000); // 30 segundos
+  
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  };
+
+  fetchData(); // Llamar a fetchData cuando el componente se monta
+  const interval = setInterval(fetchData, 10000); // Llamar a fetchData cada 10 segundos
+  alert("se envio cada 10 segundos")
 
 
   return (
